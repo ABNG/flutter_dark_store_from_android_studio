9787289e806118121365a3001f2354d0fa076424
mac <abnawaz14@gmail.com>
2020-08-18 15:38:01 +0400

    fourth commit

--- lib/reusable_widgets/appbar_icon.dart
+++ lib/reusable_widgets/appbar_icon.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dark_store/app_screens/get_help.dart';
 
@@ -5,13 +6,12 @@ class AppbarIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () => Navigator.push(context,
-          MaterialPageRoute(builder: (BuildContext context) => GetHelp())),
-      icon: Icon(
-        Icons.account_box,
-        color: Colors.black,
-        size: 30,
-      ),
-    );
+        onPressed: () => Navigator.push(context,
+            MaterialPageRoute(builder: (BuildContext context) => GetHelp())),
+        icon: Image.asset(
+          'images/consulting.png',
+          fit: BoxFit.cover,
+          width: 30,
+        ));
   }
 }
